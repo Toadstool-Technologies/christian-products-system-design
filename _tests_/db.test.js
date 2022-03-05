2e980cf1244558cb185c2cb35fbdd490f917f0e3
Christian Gorlewski <cgorlewski@gmail.com>
2022-03-05 15:38:48 -0800

    Changing Supertest to test my endpoint

--- _tests_/db.test.js
+++ _tests_/db.test.js
@@ -6,13 +6,13 @@ it('Testing to see if Jest works', () => {
   expect(1).toBe(1)
 })
 
-app.get('/test', async (req, res) => {
+app.get('/products', async (req, res) => {
   res.json({message: 'pass!'})
 });
 
 test('Should successfully send a GET request', async () => {
   try {
-    const response = await request.get('/test')
+    const response = await request.get('/products')
     expect(response.status).toBe(200)
     expect(response.body.message).toBe('pass!')
     done()
